19388b800ccaac46907b92ee43bd0ad725c82948
Sam <Sam@Brenden-PC.fios-router.home>
2022-08-01 21:41:09 -0400

    hello

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+
 public class g7{
 	public static void main(String[] args) {
 		System.out.println("hello");
 		
 	}
 }
-=======
-PEDRO 
->>>>>>> branch 'master' of https://github.com/NTKBatch5/GitClone.git
+
