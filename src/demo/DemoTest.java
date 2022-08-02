1949c344962bce1115efefa5c96c20d242b5b6f0
ntkb4 <ntkjasur@gmail.com>
2022-08-01 21:44:34 -0400

    fixed code

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,1 +1,12 @@
-PEDRO 
+package demo;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("G1 is the BEST..............");
+		System.out.println("Step something out of a lot");
+
+	}
+
+}
