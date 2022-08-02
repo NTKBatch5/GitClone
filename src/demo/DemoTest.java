00ab3d1e3f5a622cfd6286dce12ff9087aeb2454
nasrinnoorahmad <nasrinnoorahmad@nasrins-mbp.verizon.net>
2022-08-01 21:21:05 -0400

    Best Scrum Master

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -10,8 +10,11 @@ public class DemoTest {
 		System.out.println("2nd push...");
 
 		System.out.println("GO G2!!!!!!");
-		
+
 		System.out.println("G2 IS THE BEST TEAM!!!!");
+
+		System.out.println("We are hppy to have Prajota as a scrum Master!! !!!!!!");
+		System.out.println("Best scrum Master!! !!!!!!");
 
 	}
 
