b8b9cecc82d422a395554ed54ff6038b4c996b0b
Sam <Sam@Brenden-PC.fios-router.home>
2022-08-01 22:17:52 -0400

    add

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -15,9 +15,13 @@ public class DemoTest {
 		//Batch 7
 
 	}
-	
+	// add method
 	public void add(int a, int b) {
 		System.out.println(a+b);
+	}
+	
+	public void divide(int a, int b) {
+		System.out.println(a/b);
 	}
 
 	//random code.....
