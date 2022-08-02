09c1ba2686741058aec3977ff9530001797dc284
NTKBatch5 <94854058+NTKBatch5@users.noreply.github.com>
2022-08-01 21:29:01 -0400

    Update DemoTest.java
    ...

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,18 +4,7 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 
-		System.out.println("the best.");
-		System.out.println("Aymn...");
-
-		System.out.println("2nd push...");
-
-		System.out.println("GO G2!!!!!!");
-
-		System.out.println("G1 IS THE BEST......... AND we don't like Spider Man");
-		System.out.println("whoever deleted GO G2! my middle finger");
-		System.out.println("GO Prajita!!!");
-		
-		System.out.println("Douglas");
+	 
 
 	}
 
