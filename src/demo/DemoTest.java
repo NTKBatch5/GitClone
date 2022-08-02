a488d492b17b1c6088b0f70d0b51bd9c7aac945b
nasrinnoorahmad <nasrinnoorahmad@nasrins-mbp.verizon.net>
2022-08-01 21:17:53 -0400

    go team G2

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,13 +1,15 @@
 package demo;
 
 public class DemoTest {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test...");
 		System.out.println("Aymn...");
-		
+
 		System.out.println("2nd push...");
+
+		System.out.println("GO G2!!!!!!");
 
 	}
 
