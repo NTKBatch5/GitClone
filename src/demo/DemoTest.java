ccc219e3bbb4af5ef4a0489cd72408704d87547c
Sam <Sam@192.168.1.63>
2022-08-01 21:22:12 -0400

    Merge branch 'master' of https://github.com/NTKBatch5/GitClone.git

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -3,6 +3,7 @@ package demo;
 public class DemoTest {
 
 	public static void main(String[] args) {
+		System.out.println("we are the best");
 
 		System.out.println("Test...");
 		System.out.println("Aymn...");
