9a39992d322b2b70d7d6f8b934671b687f9e7bc9
BRabbit5 <110270596+BRabbit5@users.noreply.github.com>
2022-08-01 21:32:07 -0400

    Update DemoTest.java
    added code

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,14 +1,12 @@
 package demo;
 
 public class DemoTest {
-	public static void g7() {
-		
-	}
 
 	public static void main(String[] args) {
-		
 
 	 System.out.println("G1 is the BEST..............");
+	System.out.println("Step something out of a lot")
+	
 
 	}
 
