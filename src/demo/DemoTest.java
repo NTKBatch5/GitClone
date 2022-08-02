b9bf4d6b75fd12c3977cfbd68ead931bfbed8b8c
Sam <Sam@Brenden-PC.fios-router.home>
2022-08-01 21:31:17 -0400

    hello

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,8 +1,12 @@
 package demo;
 
 public class DemoTest {
+	public static void g7() {
+		
+	}
 
 	public static void main(String[] args) {
+		
 
 	 System.out.println("G1 is the BEST..............");
 
