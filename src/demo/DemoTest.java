00ce442f66bd80abbbc5faf6d57446d67d4733a7
NTKBatch5 <94854058+NTKBatch5@users.noreply.github.com>
2022-08-01 21:33:58 -0400

    Update DemoTest.java
    ..

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,17 +1,1 @@
-package demo;
-
-public class DemoTest {
-	
-	public static void g7() {
-		
-	}
-
-	public static void main(String[] args) {
-
-	 System.out.println("G1 is the BEST..............");
-	System.out.println("Step something out of a lot")
-	
-
-	}
-
-}
+Ehtisham
