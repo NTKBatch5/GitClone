6efcea8de658142602392969f1286654c85b34a2
ntkb4 <ntkjasur@gmail.com>
2022-08-01 21:56:40 -0400

    added private variable

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,11 +1,15 @@
 package demo;
 
 public class DemoTest {
+	
+	private static int num = 10;//added this line
 
 	public static void main(String[] args) {
 
 		System.out.println("G1 is the BEST..............");
 		System.out.println("Step something out of a lot");
+		
+		System.out.println(num); //added this line
 
 	}
 	
