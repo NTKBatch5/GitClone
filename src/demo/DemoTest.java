0cd394313622f55931ce451186f373fa1a64fefd
GonzaloBlondet <blondetg@gmail.com>
2022-08-04 16:18:48 -0400

    gS-codingBat

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -40,5 +40,25 @@ public class DemoTest {
 	
 	public void devBrnachTest() {
 		System.out.println("This is coming from dev branch....");
+		
 	}
+															// G0nzal0s CodingBat
+//	Start with 2 int arrays, a and b, of any length. 
+	//	Return how many of the arrays have 1 as their first element.
+		
+	public int start1(int[] a, int[] b) {
+		
+	    int count = 0;
+	    
+	    if(a.length > 0 && a[0] == 1)		// a.length > 0-- taking care of first array ( a )is any length || and seeing if first index [0] is = 1
+	        count++;						// add to the count
+	              
+	    if(b.length > 0 && b[0] == 1)		// b.length > 0-- taking care of 2nd array ( b )is any length || and seeing if first index [0] is = 1
+	        count++;						// add to the count
+	                        
+	    return count;
+	}
+
+	
+	
 }
