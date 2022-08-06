bd8bbfc9f0bc964318e5fad510e98f80bf81d136
winta <winta@DESKTOP-G9LOJV9>
2022-08-05 21:34:07 -0400

    tes code commit

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -1,32 +1,31 @@
 package demo;
 
 public class DemoTest {
-	
-	private static int num = 10;//added this line
 
 	public static void main(String[] args) {
 
 		System.out.println("G1 is the BEST..............");
 		System.out.println("Step something out of a lot");
 		
-		System.out.println(num); //added this line
 		
-		//from gitHUB
-		//Batch 7 
-		// ...
+
+		System.out.println(num); // added this line
+
+		// from gitHUB
+		// Batch 7
 
 	}
+
 	// add method
 	public void add(int a, int b) {
-		System.out.println(a+b);
+		System.out.println(a + b);
 	}
-	
+
 	public void divide(int a, int b) {
-		System.out.println(a/b);
-		
-		
+		System.out.println(a / b);
+
 		System.out.println("G7...");
-		
+
 	}
 
 	//random code.....
